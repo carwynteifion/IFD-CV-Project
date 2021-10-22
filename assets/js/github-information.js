52e6ae34dbc3f926153e13369c5b642b1898ada9
carwynteifion <carwynteifion@yahoo.co.uk>
2021-10-22 21:57:59 +0000

    Fix bugs for gh-user-data and gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,12 +2,12 @@ function userInformationHTML(user) {
     return `
         <h2>${user.name}
             <span class="small-name">
-                (@<a href="${user.html_url}" target="_blank">${user.login}</a>
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
             <div class="gh-avatar">
-                <a href="${user.html_url} target="_blank">
+                <a href="${user.html_url}" target="_blank">
                     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}">
                 </a>
             </div>
@@ -18,25 +18,27 @@ function userInformationHTML(user) {
 function repoInformationHTML(repos) {
     if(repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
-
-        var listItemsHTML = repos.map(function(repo) {
-            return `<li>
-                        <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-                    </li>`;
-        });
-
-        return `<div class="clearfix repo=list">
-                    <p>
-                        <strong>Repo List:</strong>
-                    </p>
-                    <ul>
-                        ${listItemsHTML.join("\n")}
-                    </ul>
-                </div>`;
     }
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo=list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -69,3 +71,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
